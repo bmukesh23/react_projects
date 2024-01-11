2a821557b96163dbe32ee34044b69f7c2f3ac00d
Mukesh <billamukesh1234@gmail.com>
2024-01-11 22:36:20 +0530

    11-01-24 reactjs

--- accordion/src/SingleQuestion.jsx
+++ accordion/src/SingleQuestion.jsx
@@ -6,16 +6,18 @@ const SingleQuestion = ({ title, info }) => {
 
     const [showInfo, setShowInfo] = useState(false);
 
+    () => { }
+
     return (
         <article className="question">
             <header>
                 <h5>{title}</h5>
                 <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
-                    <AiOutlinePlus />
+                    {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
                 </button>
             </header>
             {showInfo && <p>{info}</p>}
-        </article>
+        </article >
     )
 }
 export default SingleQuestion
