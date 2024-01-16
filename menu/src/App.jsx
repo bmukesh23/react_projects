0bb80f28d54ada7302439cd619b96a8007b0095f
Mukesh <billamukesh1234@gmail.com>
2024-01-16 22:26:28 +0530

    16-01-2024

--- menu/src/App.jsx
+++ menu/src/App.jsx
@@ -2,15 +2,33 @@ import { useState } from "react";
 import Title from "./Title";
 import menu from './data';
 import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all', ...tempSet]
+
+
+const allCategories = ['all', ...new Set(menu.map(item => item.category))]
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
 
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      return setMenuItems(menu);
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
 
   return (
     <main>
       <section className="menu">
         <Title task='Our Menu' />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
