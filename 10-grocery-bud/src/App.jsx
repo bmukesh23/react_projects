a78d69569bc9654d15fbe8d3c51f1f7f05940585
Mukesh <billamukesh1234@gmail.com>
2024-01-19 17:57:23 +0530

    19-01-24 commit

--- 10-grocery-bud/src/App.jsx
+++ 10-grocery-bud/src/App.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+import Form from "./Form";
+import { nanoid } from "nanoid";
+import Items from "./Items";
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
+  const [items, setItems] = useState([]);
+
+  const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    }
+    setItems([...items, newItem]);
+  }
+
+  return (
+    <section className="section-center">
+      <Form addItem={addItem} />
+      <Items/>
+    </section>
+  );
 };
 
 export default App;
--- 10-grocery-bud/src/Form.jsx
+++ 10-grocery-bud/src/Form.jsx
@@ -1,6 +1,30 @@
-const Form = () => {
-  return (
-    <div>Form</div>
-  )
+import { useState } from "react"
+
+const Form = ({ addItem }) => {
+    const [newItemName, setNewItemName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!newItemName) {
+            return;
+        }
+        addItem(newItemName);
+        setNewItemName('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h4>Grocery Bud</h4>
+            <div className="form-control">
+                <input
+                    type="text"
+                    className="form-input"
+                    value={newItemName}
+                    onChange={(event) => setNewItemName(event.target.value)}
+                />
+                <button type="submit" className="btn">add item</button>
+            </div>
+        </form>
+    )
 }
 export default Form
