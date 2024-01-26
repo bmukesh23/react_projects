613ceb7995992eff0760d363d3733792035ecc81
Mukesh <billamukesh1234@gmail.com>
2024-01-26 20:57:13 +0530

    26-01-24 commit

--- 13-task_bud/frontend/src/Items.jsx
+++ 13-task_bud/frontend/src/Items.jsx
@@ -1,32 +1,22 @@
-import { useQuery } from '@tanstack/react-query';
 import SingleItem from './SingleItem';
-import customFetch from './utils';
+import { useFetchTasks } from './reactQueryCustomHooks';
 
 const Items = () => {
-  const { isLoading, isError, data, error } = useQuery({
-    queryKey: ['tasks'],
-    queryFn: () => customFetch.get('/'),
-  });
+  const { isLoading, isError, data } = useFetchTasks();
 
   if (isLoading) {
-    return <p style={{ marginTop: '1rem' }}>Loading...</p>
+    return <p style={{ marginTop: '1rem ' }}>Loading...</p>;
   }
-
   if (isError) {
-    return <p style={{ marginTop: '1rem' }}>There is an Error</p>
+    return <p style={{ marginTop: '1rem ' }}>There was an error...</p>;
   }
-
-  // if (error) {
-  //   return <p style={{ marginTop: '1rem' }}>{error.message}</p>
-  // }
 
   return (
     <div className='items'>
-      {data.data.taskList.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
   );
 };
-
 export default Items;
